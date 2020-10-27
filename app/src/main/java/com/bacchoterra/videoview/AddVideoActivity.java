8504ad8b5eb55cce7a3a01cb1a9f50bacd4003ee
Bruno Baccho Terra <brunobacchot@hotmail.com>
2020-10-27 15:23:06 -0300

    Initial commit

---
+++ app/src/main/java/com/bacchoterra/videoview/AddVideoActivity.java
@@ -0,0 +1,15 @@
+package com.bacchoterra.videoview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class AddVideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_video);
+    }
+}
